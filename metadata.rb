2e95698e1f658877463322ba648ba6b2ce70e502
Jimmy Hillis <jimmy@hillis.me>
2014-05-07 22:51:14 +0800

    Set correct tag

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jimmy@hillis.me"
 license          "MIT"
 description      "Installs and configures Wordpress running under nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.5"
+version          "0.1.0"
 
 depends "git"
 depends "database"
